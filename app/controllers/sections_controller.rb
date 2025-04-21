c1ca81f7dd47916c1375b6ec2bbae0f5905f930e
Sebastian Olivares <olivares.heredia@gmail.com>
2025-04-21 12:17:04 +0200

    created Projects, Sections and Tasks controllers

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,22 @@
+class SectionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'sections/index'
+  get 'sections/show'
+  get 'sections/new'
+  get 'sections/create'
+  get 'sections/edit'
+  get 'sections/update'
+  get 'sections/destroy'
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/new'
+  get 'projects/create'
+  get 'projects/edit'
+  get 'projects/update'
+  get 'projects/destroy'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
