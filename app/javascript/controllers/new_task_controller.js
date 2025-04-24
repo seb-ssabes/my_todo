6905a0b5d6219e46b6d240c519b03d87a6bdc80d
Sebastian Olivares <olivares.heredia@gmail.com>
2025-04-24 16:26:59 +0200

    added a toggle between plus and exit icons

--- app/javascript/controllers/new_task_controller.js
+++ app/javascript/controllers/new_task_controller.js
@@ -2,9 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-task"
 export default class extends Controller {
-  static targets = ["newTaskForm"]
+  static targets = ["newTaskForm", "addIcon"]
 
   toggleForm() {
+    const addIcon = this.addIconTarget.classList
+    addIcon.add("opacity-0")
+
+    setTimeout(() => {
+      if (addIcon.contains("fa-plus")) {
+        addIcon.remove("fa-plus")
+        addIcon.add("fa-xmark", "px-0.5")
+      } else {
+        addIcon.remove("fa-xmark", "px-0.5")
+        addIcon.add("fa-plus")
+      }
+      addIcon.remove("opacity-0")
+    }, 150);
+
     this.newTaskFormTarget.classList.toggle("translate-x-full")
     this.newTaskFormTarget.classList.toggle("translate-x-0")
   }
